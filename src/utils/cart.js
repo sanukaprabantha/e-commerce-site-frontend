78ce9347c13eda29bbaeb6df006df878146462d6
sanukathennakoon20@gmail.com <sanukathennakoon20@gmail.com>
2026-01-24 00:22:56 +0530

    loadCart Function added

---
+++ src/utils/cart.js
@@ -0,0 +1,52 @@
+export default loadCart()
+{
+    let cartString=localStorage.getItem("cart"); // cart is stored as a JSON string
+     
+    if(cartString==null)
+    {
+        localStorage.setItem("cart","[]");
+    }
+    let  cart=JSON.parse(cartString);// convert the string(cartString) in local storage into array
+
+    return cart;
+}
+
+export function addToCart(productId,quantity){
+    const cart = loadCart();
+    const existingItemIndex = cart.findIndex(
+        (item )=> {
+            return item.productId === product.productId
+        }
+    );
+    if (existingItemIndex == -1) {
+        // Item not in cart
+        if(quantity<1){
+            console.log("Quantity must be at least 1");
+            return;
+        }
+
+        const cartItem = {
+            productId: product.productId,
+            name: product.name,
+            price: product.labelPrice,
+            image: product.images[0],
+            quantity: quantity
+
+        }
+        cart.push(cartItem);
+    }
+    else{
+        // Item already in cart
+        const existingItem = cart[existingItemIndex];
+        const newQuantity = existingItem.quantity + quantity;
+        if(newQuantity<1){
+            cart = cart.fileter((item)=>{
+                return item.productId !== product.productId
+            });
+        }
+        else{
+            existingItem.quantity = newQuantity;
+        }
+    }
+    localStorage.setItem("cart",JSON.stringify(cart));
+}
